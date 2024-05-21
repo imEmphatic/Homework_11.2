2704eb563410ad96436fe3454b718ea069e22cf8
Aleksey Prusikov <imemphatic@yandex.ru>
2024-05-21 13:50:13 +0300

    add new func

---
+++ str_func.py
@@ -0,0 +1,8 @@
+def main():
+    """"first func"""
+    user_input = str(input("Введите текст для вывода с заглавными буквами: "))
+    print(user_input.upper())
+
+
+if __name__ == "__main__":
+    main()
